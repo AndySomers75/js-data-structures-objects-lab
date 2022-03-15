70d8af6e55625f4bb03c1ec99eabfb84360ccfbc
Andrew <andrew@pureaquasprings.com>
2022-03-15 11:16:04 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, address, value ) {  
+  // Alternate using ES6 Spread operators:
+  // return { ...driver, ...{ [key]: value } }
+  return Object.assign({}, driver, { address: "11 Broadway" });
+}
+ 
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, address, value ) {
+  
+  driver [address] = '12 Broadway';
+
+  return driver;
+}
+
+
+function deleteFromDriverByKey(driver, key) {
+  // Alternate using ES6 Spread operators:
+  // const newObj = { ...driver }
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+
+  return driver;
+}
